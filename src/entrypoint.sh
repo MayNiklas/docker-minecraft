59fb6fd950fd3b45c6d1f10cc408f3812b3bfb3a
MayNiklas <info@niklas-steffen.de>
2020-11-04 23:25:20 +0100

    ↗️ Added entrypoint.sh: installing mc & setting permissions

---
+++ src/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FILE=""
+DIR="/app"
+if [ "$(ls -A $DIR)" ]; then
+     echo "$DIR is not Empty. Minecraft is allready installed"
+else
+    echo "$DIR is Empty. Minecraft is going to get installed"
+    wget $server.jar-url
+    java -jar server.jar
+    sed -i 's/false/true/g' eula.txt 
+fi
+
+# Set groups and permissions
+groupadd java -g ${GID:-1000}
+useradd -u ${UID:-1000} -g ${GID:-1000} java
+chown -R  java:java /app
+
+su java -c 'java -Xmx$MC_RAM -jar server.jar'
